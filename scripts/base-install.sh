0bc2b8d7d16c3f78aa8d2ddbf1da7cd3b25d31fd
Myute Branch <knimmalacloud@gmail.com>
2023-09-03 21:15:34 -0500

    adding dotfiles

---
+++ scripts/base-install.sh
@@ -0,0 +1,7 @@
+# install lazy git
+go install github.com/jesseduffield/lazygit@latest
+# install zsh
+sudo apt install zsh
+# install oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
