a9b622aa6167547240e9e179d457f522d9f80ce5
MyuteBranch <kaushiknimmala@gmail.com>
2024-02-25 10:31:46 -0600

    added arch tools install

---
+++ install_scripts/linux_install/install_arch.sh
@@ -0,0 +1,10 @@
+# update packages
+sudo pacman -Syu
+# install base tools
+sudo pacman -S ripgrep btop go nodejs npm zsh arandr
+# install i3
+sudo pacman -S i3-wm i3blocks i3lock picom dmenu lxappearance feh ttf-font-awesome 
+# install sound stuff
+sudo pacman -S pamixer 
+# install other tools
+sudo pacman -S firefox chromium libstdc++ webkit2gtk network-manager-applet wpa_supplicant wireless_tools
