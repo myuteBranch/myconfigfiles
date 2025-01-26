0522f717e3f363ee3ae370b69002d102273b915a
myuteBranch <kaushiknimmala@gmail.com>
2025-01-26 10:48:14 -0600

    update lazyvim config to be soft linkable

--- dotfiles/lazynvim/lua/plugins/custom/colorscheme.lua
+++ dotfiles/lazynvim/lua/plugins/custom/colorscheme.lua
@@ -4,7 +4,7 @@ return {
     lazy = false,
     priority = 1000,
     opts = function(_, opts)
-      opts.transparent = false
+      opts.transparent = true
       opts.italic_comments = true
       opts.borderless_telescope = false
     end,
