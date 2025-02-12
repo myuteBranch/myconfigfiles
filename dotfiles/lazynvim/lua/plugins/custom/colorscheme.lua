32c52d6ce4a579137cc7ec28808ddc491a25de42
myuteBranch <kaushiknimmala@gmail.com>
2025-02-11 18:27:24 -0600

    added jj config and minor changes to other scripts

--- dotfiles/lazynvim/lua/plugins/custom/colorscheme.lua
+++ dotfiles/lazynvim/lua/plugins/custom/colorscheme.lua
@@ -1,18 +1,16 @@
 return {
-  {
-    "scottmckendry/cyberdream.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = function(_, opts)
-      opts.transparent = true
-      opts.italic_comments = true
-      opts.borderless_telescope = false
-    end,
-  },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "cyberdream",
-    },
-  },
+	{
+		"ellisonleao/gruvbox.nvim",
+		priority = 1000,
+		config = true,
+		opts = {
+			transparent_mode = true,
+		},
+	},
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "gruvbox",
+		},
+	},
 }
--- scripts/tmux_session.sh
+++ scripts/tmux_session.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-
 path=$1
 project=$(basename $path)
 echo $project
 tmux new -d -s $project -c $path
+
 window=1
+tmux new-window -t $project:$window -n 'git' -c $path
+tmux send-keys -t $project:$window "lazygit" C-m
+
+window=2
 tmux rename-window -t $project:$window 'nvim'
 tmux send-keys -t $project:$window "nvim ." C-m
 
-window=2
+window=3
 tmux new-window -t $project:$window -n 'term' -c $path
 tmux send-keys -t $project:$window "ls -lra" C-m
-
-window=3
-tmux new-window -t $project:$window -n 'git' -c $path
-tmux send-keys -t $project:$window "lazygit" C-m
